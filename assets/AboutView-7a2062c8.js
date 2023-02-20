b24d16b4dddbc1a64bf450ed864a8f5508054e40
johnson <you@example.com>
2023-02-21 00:04:03 +0800

    deploy

---
+++ assets/AboutView-7a2062c8.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t,d as c}from"./index-169d79b8.js";const s={},n=c("h1",null,"about",-1),_=[n];function a(r,d){return o(),t("div",null,_)}const i=e(s,[["render",a]]);export{i as default};
