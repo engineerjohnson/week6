e611feb1167530d3674143270f491e7c653f9e03
johnson <you@example.com>
2023-02-23 23:39:15 +0800

    add-ProductsView

--- src/views/front/ProductsView.vue
+++ src/views/front/ProductsView.vue
@@ -1,4 +1,5 @@
 <script>
+import Loading from "vue-loading-overlay";
 const { VITE_APP_API, VITE_APP_PATH } = import.meta.env;
 import { RouterLink } from "vue-router";
 export default {
@@ -9,22 +10,45 @@ export default {
       cart: [],
       loadingId: "",
       user: {},
-      isLoading: true,
+      isLoading: false,
     };
   },
   components: {
     RouterLink,
+    Loading,
   },
   //方法
   methods: {
     getProducts() {
+      this.isLoading = true;
       this.$http
         .get(`${VITE_APP_API}/v2/api/${VITE_APP_PATH}/products`)
         .then((res) => {
           this.products = res.data.products;
+          this.isLoading = false;
         })
         .catch((err) => {
           alert(err.response.data.message);
+        });
+    },
+    addToCart(product_id, qty = 1) {
+      //傳入商品ID及數量 數量預設為1
+      const data = {
+        product_id,
+        qty,
+      };
+      this.loadingId = product_id;
+      this.isLoading = true;
+      this.$http
+        .post(`${VITE_APP_API}/api/${VITE_APP_PATH}/cart`, { data })
+        .then((res) => {
+          alert(res.data.message);
+          // this.$refs.modalProductHide.hide();
+          this.loadingId = "";
+          this.isLoading = false;
+        })
+        .catch((err) => {
+          alert(err);
         });
     },
   },
@@ -35,45 +59,57 @@ export default {
 </script>
 
 <template>
-  <div>
-    <div class="container">
-      <div class="row py-3">
-        <div class="col-md-6">
-          <h2>產品列表</h2>
-          <table class="table table-hover mt-4">
-            <thead>
-              <tr>
-                <th width="150">產品名稱</th>
-                <th width="120">原價</th>
-                <th width="120">售價</th>
-                <th width="150">是否啟用</th>
-                <th width="120">查看細節</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="item in products" :key="item.id">
-                <td width="150">{{ item.title }}</td>
-                <td width="120">
-                  {{ item.origin_price }}
-                </td>
-                <td width="120">
-                  {{ item.price }}
-                </td>
-                <td width="150">
-                  <span v-if="item.is_enabled" class="text-success">啟用</span>
-                  <span v-else>未啟用</span>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <p>
-            目前有 <span>{{ products.length }}</span> 項產品
-          </p>
-        </div>
-        <div class="col-md-6">
-          <RouterLink :to="`/Product/${products.id}`">單一產品細節</RouterLink>
-        </div>
-      </div>
-    </div>
+  <loading v-model:active="isLoading" />
+  <div class="container">
+    <table class="table align-middle">
+      <thead>
+        <tr>
+          <th>圖片</th>
+          <th>商品名稱</th>
+          <th>價格</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in products" :key="product.id">
+          <td style="width: 10%">
+            <div
+              style="padding-top: 100%; background-size: contain"
+              :style="{ backgroundImage: `url(${product.imageUrl})` }"
+            ></div>
+          </td>
+          <td>
+            {{ product.title }}
+          </td>
+          <td>
+            <div class="h5" v-if="product.price === product.origin_price">
+              {{ product.price }} 元
+            </div>
+            <div v-else>
+              <del class="h6">原價 {{ product.origin_price }} 元</del>
+              <div class="h5">現在只要 {{ product.price }} 元</div>
+            </div>
+          </td>
+          <td>
+            <div class="btn-group btn-group-sm">
+              <button type="button" class="btn btn-outline-secondary">
+                <router-link :to="`/Product/${product.id}`"
+                  ><i class="fa-pulse text-end">查看更多</i></router-link
+                >
+              </button>
+              <button
+                type="button"
+                class="btn btn-outline-danger"
+                @click="addToCart(product.id)"
+                :disabled="product.id === loadingId"
+              >
+                <i class="f fa-pulse"></i>
+                加到購物車
+              </button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
